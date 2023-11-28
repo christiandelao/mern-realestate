ba3662382f23bf0ab4daa7890a208b7ba9f8cc10
Christian Delao <christiandelao10@.gmail.com>
2023-11-27 21:37:31 -0800

    Sign up functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -8,7 +8,7 @@ export const signup = async (req,res,next)=>{
     const newUser = new User({username,email,password: hashedPassword}); 
     try{
         await newUser.save()
-        res.status(201).json('User created!')
+        res.status(201).json('User created!');
     } catch (error){
         next(error);
     }
