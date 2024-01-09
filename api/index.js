871eb4e96f0bdf65f5ba758b8cc733ed4414db49
Christian Delao <christiandelao10@.gmail.com>
2024-01-08 18:28:14 -0800

    Project Complete

--- api/index.js
+++ api/index.js
@@ -4,6 +4,12 @@ import dotenv from 'dotenv';
 import userRouter from './routes/user.route.js'
 dotenv.config();
 import authRouter from './routes/auth.route.js'
+import listingRouter from './routes/listing.route.js'
+import cookieParser from 'cookie-parser';
+import path from 'path';
+dotenv.config();
+
+
 mongoose.connect(process.env.MONGO).then(()=>{
     console.log('Connection Succesful!');
 }).catch((err)=>{
@@ -13,15 +19,23 @@ const app = express();
 
 app.use(express.json());
 
+app.use(cookieParser());
+
 app.listen(3000, ()=>{
     console.log('Server is running on port 3000!');
 }
 );
 
+const __dirname = path.resolve();
+
 app.use("/api/user", userRouter);
 app.use('/api/auth', authRouter);
+app.use('/api/listing', listingRouter);
 
-
+app.use(express.static(path.join(__dirname, '/client/dist')));
+app.get('*', (req,res)=>{
+    res.sendFile(path.join(__dirname, '/client', 'dist', 'index.html'));
+})
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
     const message = err.message || 'Internal Server Error';
